b6a8adced7e47240bbdf60b6923c6cacf329454b
lexamenrf44 <alexander.stcstudent@gmail.com>
2024-10-26 11:24:47 +0200

    Minor array fixes

--- src/main/java/tech_tasks/array/FindIndicesThatAddUpToTargetViaHashMapMethodCall.java
+++ src/main/java/tech_tasks/array/FindIndicesThatAddUpToTargetViaHashMapMethodCall.java
@@ -12,7 +12,7 @@ public class FindIndicesThatAddUpToTargetViaHashMapMethodCall {
 
     public static void main(String[] args) {
 
-        int[] nums = {2, 7, 11, 15};
+        int[] nums = {2, 11, 7, 15};
         int target = 9;
 
         int[] result = HelpMethods.findTwoSumHashMap(nums, target);
