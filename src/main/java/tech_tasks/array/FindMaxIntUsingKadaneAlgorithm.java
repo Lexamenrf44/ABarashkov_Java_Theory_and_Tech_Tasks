71336d180314a8b30f556ec6ed94655566082288
lexamenrf44 <alexander.stcstudent@gmail.com>
2024-10-26 19:12:42 +0200

    Implemented Kadane's Algorithm method usage for array with negative integers and assertions

---
+++ src/main/java/tech_tasks/array/FindMaxIntUsingKadaneAlgorithm.java
@@ -0,0 +1,23 @@
+package tech_tasks.array;
+
+import helpers.HelpMethods;
+
+/*
+
+Find maximum value in array using Kadane's Algorithm
+
+*/
+
+public class FindMaxIntUsingKadaneAlgorithm {
+    public static void main(String[] args) {
+        int[] nums = {2, -7, 11, -15};
+        int max = HelpMethods.findMaxSubArray(nums);
+
+        if (max == 11) {
+            assert true;
+            System.out.println("Test passed! Maximum sum of contiguous subarray: " + max);
+        } else {
+            assert false : "Test failed! Maximum sum of contiguous subarray: " + max;
+        }
+    }
+}
