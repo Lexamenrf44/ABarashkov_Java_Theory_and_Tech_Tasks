91d57494134ea03be43fbeac641ffbfd72450098
lexamenrf44 <alexander.stcstudent@gmail.com>
2024-10-30 13:07:04 +0100

    Migrated FindMaxMethods to a separate java class

--- src/main/java/helpers/HelpMethods.java
+++ src/main/java/helpers/HelpMethods.java
@@ -56,22 +56,6 @@ public class HelpMethods {
         }
     }
 
-    // Brute force method to find the maximum value in an array
-    public static int findMaxBruteForce(int[] array) {
-
-        int max = array[0];  // Assume the first element is the largest for now
-
-        // Loop through the rest of the array
-        for (int i = 1; i < array.length; i++) {
-            // Check if the current element is greater than the current max
-            if (array[i] > max) {
-                max = array[i];  // Update max if we found a bigger number
-            }
-        }
-
-        return max;  // Return the largest number found
-    }
-
     // Method to find the previous value in an array for a given index
     public static int findPrevious(int[] array, int index) {
 
@@ -126,28 +110,4 @@ public class HelpMethods {
         return inputArray[indexOfMaximum] - inputArray[indexOfMinimum];
     }
 
-    // Method to find maximum subarray - Kadane's Algorithm
-    public static int findMaxSubArray(int[] nums) {
-
-        // Initialize current sum and max sum variables
-        int currentSum = 0;
-        int max = Integer.MIN_VALUE;
-
-        // Loop through each element in the array
-        for (int i = 0; i < nums.length; i++) {
-            // Add the current element to the current sum
-            currentSum += nums[i];
-
-            // Update max sum if the current sum is greater
-            max = Math.max(max, currentSum);
-
-            // If current sum drops below 0, reset it to 0
-            if (currentSum < 0) {
-                currentSum = 0;
-            }
-        }
-
-        // Return the maximum sum found
-        return max;
-    }
 }
