bc19de68c3bee00da6ddd10b7045f49ff1de3b03
lexamenrf44 <alexander.stcstudent@gmail.com>
2024-10-30 13:27:05 +0100

    Method naming refactoring to understand the approach;

--- src/main/java/tech_tasks/array/find/difference/ViaLinearScan.java
+++ src/main/java/tech_tasks/array/find/difference/ViaLinearScan.java
@@ -1,21 +1,21 @@
-package tech_tasks.array;
+package tech_tasks.array.find.difference;
 
 import helpers.*;
 
 /*
 
-Find the difference between its maximum and minimum elements in array calling a method
+Find the difference between its maximum and minimum in array calling a linear scan method
 
 */
 
-public class FindDifferenceViaMethodCall {
+public class ViaLinearScan {
 
     public static void main(String[] args) {
 
         int[] arr = {3, 5, 1, 8, 7};
         int target = 8;
 
-        int difference = HelpMethods.CalculateArrayDifference(arr);
+        int difference = HelpMethods.findArrayDifferenceLinearScan(arr);
 
         if (difference == 8) {
             assert true;
