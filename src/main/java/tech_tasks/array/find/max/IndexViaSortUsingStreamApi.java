cf319040693d498499622d355eacf4a24bed98bd
lexamenrf44 <alexander.stcstudent@gmail.com>
2024-11-21 22:15:41 +0100

    Minor formatting fix;

--- src/main/java/tech_tasks/array/find/max/IndexViaSortUsingStreamApi.java
+++ src/main/java/tech_tasks/array/find/max/IndexViaSortUsingStreamApi.java
@@ -13,7 +13,6 @@ public class IndexViaSortUsingStreamApi {
     public static void main(String[] args) {
 
         int[] arr = {4, 2, 9, 1, 5, 6};
-
         int max = Arrays.stream(arr).max().getAsInt();
 
         Arrays.sort(arr);
