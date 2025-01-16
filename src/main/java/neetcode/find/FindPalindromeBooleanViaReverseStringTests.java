15dfd01005e767bc6a0ae963365dcc03b3978935
lexamenrf44 <alexander.stcstudent@gmail.com>
2025-01-16 17:02:06 +0100

    Classes naming minor fixes;

--- src/main/java/neetcode/find/FindPalindromeBooleanViaReverseStringTests.java
+++ src/main/java/neetcode/find/FindPalindromeBooleanViaReverseStringTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-public class FindPalindromeTests {
+public class FindPalindromeBooleanViaReverseStringTests {
 
     @Test
     void validPalindromeTest() {
